2554dac421fd736b22f89680955ad52df03ead85
Gade Rushikesh <143445674+GadeRushi0202@users.noreply.github.com>
2024-02-14 10:24:37 +0530

    Add project files.

---
+++ CuriousNumber.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarahiAssignment
+{
+    internal class CuriousNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number :- ");
+            string numStr = Console.ReadLine();
+            int sum1 = 0;
+
+            foreach (char c in numStr)
+            {
+                int digit = (int)Char.GetNumericValue(c);
+                sum1 += Factorial(digit);
+            }
+
+            int num = int.Parse(numStr);
+
+            if (sum1 == num)
+            {
+                Console.WriteLine("Sum of factorial of " + num + " is " + sum1);
+            }
+            else
+            {
+                Console.WriteLine("Sum of factorial of " + num + " is " + sum1);
+            }
+        }
+
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Factorial(n - 1);
+        }
+    }
+}
---
+++ ExponentialNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarahiAssignment
+{
+    internal class ExponentialNumber
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> distinctTerm = new HashSet<int>();
+
+            for (int a = 2; a <= 5; a++)
+            {
+                for (int b = 2; b <= 5; b++)
+                {
+                    int result = (int)Math.Pow(a, b);
+                    distinctTerm.Add(result);
+                }
+            }
+            int[] sortedDistinctTerms = new int[distinctTerm.Count];
+            distinctTerm.CopyTo(sortedDistinctTerms);
+            Array.Sort(sortedDistinctTerms);
+
+            Console.WriteLine("Sequence of distinct terms:");
+            foreach (int term in sortedDistinctTerms)
+            {
+                Console.WriteLine(term);
+            }
+        }
+    }
+}
